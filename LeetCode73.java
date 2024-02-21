f0fd596643bdfcac083b165047550c25e8c642ea
Aman Kumar <91582409+Aman1953@users.noreply.github.com>
2024-02-21 18:30:23 +0530

    Set Matrix Zeroes

--- LeetCode73.java
+++ LeetCode73.java
@@ -1,35 +1,22 @@
-// Brute Force
-
 class Solution {
     public void setZeroes(int[][] matrix) {
-int length_row = matrix.length;
-        int length_column = matrix[0].length;
-        int[] row = new int[length_row];
-        int[] column = new int[length_column];
-        Arrays.fill(row, 1);
-        Arrays.fill(column, 1);
-
-
-        for (int i = 0; i < length_row; i++) {
-            for (int j = 0; j < length_column; j++) {
-                if (matrix[i][j] == 0){
-                    row[i] = 0;
-                column[j] = 0;}
+        int []row = new int[matrix.length];
+        int []column = new int [matrix[0].length];
+        Arrays.fill(row,1);
+        Arrays.fill(column,1);
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==0){
+                    row[i]=0;
+                    column[j]=0;
+                }
             }
         }
-
-
-        for (int i = 0; i < row.length; i++) {
-            for (int j = 0; j < column.length; j++) {
-                if (row[i] == 0 || column[j] == 0)
-                    matrix[i][j] = 0;
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(row[i]==0 || column[j]==0)
+                    matrix[i][j]=0;
             }
-        }
-        for (int i = 0; i < length_row; i++) {
-            for (int j = 0; j < length_column; j++) {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
         }
     }
 }
